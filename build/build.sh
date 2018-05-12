655ea20945ab8e6a36816ed45f8b583e0fc39f38
sweagraff <steve.weagraff@gmail.com>
2018-05-12 11:26:18 -0400

    small repairs

--- build/build.sh
+++ build/build.sh
@@ -20,14 +20,7 @@ done
 
 if [[ $performDownload -eq 1 ]]; then
 	echo "Downloading latest Pharo"
-	#curl get.pharo.org/64/stable+vm | bash
-	#wget -O- get.pharo.org/64 | bash
-	curl get.pharo.org/64/70+vm | bash
-	#curl http://get.pharo.org/64/60 | bash
-	#curl get.pharo.org/60+vm | bash
-
-	#curl http://get.pharo.org/64/vm60 | bash
-	#curl http://get.pharo.org/64/60 | bash
+  curl get.pharo.org | bash
 fi
 
 ./pharo Pharo.image st setup.st --save --quit
